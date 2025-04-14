32602f591a4734c2736ed4c19422b8548b7e0056
Kapilesh Rajput <rajputkapilesh@outlook.com>
2025-04-14 19:58:43 +0530

    Initial Commit

---
+++ ScambleWords/ScambleWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScambleWordsApp.swift
+//  ScambleWords
+//
+//  Created by Kapilesh Rajput on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ScambleWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
